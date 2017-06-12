d8aa18e124580e335ee160906e0faa041dcd56aa
Tushar Kale <tushar.K197@gmail.com>
2017-06-13 02:23:48 +0530

    url bug fix 3

--- src/app/get-data.service.ts
+++ src/app/get-data.service.ts
@@ -10,14 +10,14 @@ export class GetDataService {
     	private http: Http
   	)	 	{}
   	getJsonData(route) {
-    	return this.http.get('https://ec2-13-58-171-21.us-east-2.compute.amazonaws.com'+route).map(response => response.json());
+    	return this.http.get('ec2-13-58-171-21.us-east-2.compute.amazonaws.com'+route).map(response => response.json());
   	}
 
   	postData(route,data){
   		let headers = new Headers({ 'Content-Type': 'text/plain'});
     	let options = new RequestOptions({ headers: headers });
 
-    	return this.http.post('https://ec2-13-58-171-21.us-east-2.compute.amazonaws.com'+route, data, options).map(response => response.json());
+    	return this.http.post('ec2-13-58-171-21.us-east-2.compute.amazonaws.com'+route, data, options).map(response => response.json());
   	}
 
     
